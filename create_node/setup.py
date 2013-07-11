31db060c6d60adc7e98ad993dfb5efdab5f7f31c
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-07-11 09:19:24 +0900

    create_node: fixes python script install paths

--- create_node/setup.py
+++ create_node/setup.py
@@ -4,9 +4,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(packages=['create_node'],
-                             package_dir={'': 'src'},
-                             scripts=['scripts/kinect_breaker_enabler.py']
-#                             requires=['roslib', 'rospy']
-)
+                             package_dir={'': 'src'})
 
 setup(**d)
