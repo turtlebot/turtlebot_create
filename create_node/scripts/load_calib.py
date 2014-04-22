8bd00db0498150b06200d9e30e94ff7515fe8fab
Kenneth Bogert <kbogert@uga.edu>
2014-04-22 00:35:43 -0400

    Attempt to use the usb to serial converter's id
    If the robot receives a velocity command we'll assume that it's been
    brought up and must now move.  If we don't have the kinect's serial number
    yet assume we won't ever get it.  Instead attempt to find a calibration file
    that matches the serial number of the usb to serial converter that should be
    attached at /dev/ttyUSB0.  This setup will prevent the calibration script from
    running indefinitely waiting for a kinect that might never come.

--- create_node/scripts/load_calib.py
+++ create_node/scripts/load_calib.py
@@ -13,10 +13,40 @@ import dynamic_reconfigure.client
 import subprocess
 
 service = 'turtlebot_node/set_operation_mode'
+cmd_vel_received = False
+
+
+def cmd_vel_callback(msg):
+    global cmd_vel_received
+    cmd_vel_received = True
+
+def get_usb_to_serial_id():
+    usbpath = subprocess.check_output("readlink -f /sys/class/tty/ttyUSB0", shell=True)
+    if len(usbpath.strip()) == 0:
+        return None
+    serialid = ""
+    try:
+        f = open(usbpath + "../../../../serial", "r")
+        serialid = f.read().strip()
+        f.close()
+    except:
+        pass
+    try:
+        f = open(usbpath + "../../../../idVendor", "r")
+        serialid += f.read().strip()
+        f.close()
+        f = open(usbpath + "../../../../idProduct", "r")
+        serialid += f.read().strip()
+        f.close()
+    except:
+        pass
+    if len(serialid.strip()) == 0:
+        return None
+    return serialid
 
 def get_kinect_serial():
     ret = subprocess.check_output("lsusb -v -d 045e:02ae | grep Serial | awk '{print $3}'", shell=True)
-    if len(ret) > 0:
+    if len(ret.strip()) > 0:
         return ret.strip()
     return None
 
@@ -31,6 +61,8 @@ def load_yaml(path):
 
 if __name__ == '__main__':
     rospy.init_node('calibration_loader')
+
+    cmd_vel_sub = rospy.Subscriber('cmd_vel', rospy.AnyMsg, cmd_vel_callback)
     
     # wait for the main create node to come up (no point in using dynamic reconfigure until then)
     rospy.wait_for_service(service)
@@ -38,7 +70,7 @@ if __name__ == '__main__':
     # wait for a kinect to appear
 
     kinect_serial = None
-    while not rospy.is_shutdown() and kinect_serial is None:
+    while not rospy.is_shutdown() and kinect_serial is None and not cmd_vel_received:
         try:
             kinect_serial = get_kinect_serial()
         except:
@@ -46,6 +78,13 @@ if __name__ == '__main__':
 
     if rospy.is_shutdown():
         exit(0)
+
+    if kinect_serial is None:
+        rospy.loginfo("Velocity command received, assuming the robot is operational.  Attempting to calibration based on the usb to serial convert's id")
+        kinect_serial = get_usb_to_serial_id()
+        if kinect_serial is None:
+            rospy.logwarn("Calibration failed, no unique identifier for the robot could be found.")
+            exit(0) # well, at least we tried
 
     rospack = rospkg.RosPack()
 
